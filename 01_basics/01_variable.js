31e9b62901741bc23d223d306a43ce962cc22fe6
Tanmoy05 <tanmoymukherjee746@gmail.com>
2025-10-19 08:22:54 +0530

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 133423;
+let accountEmail = "tanmoy@gmail.com";
+var accountPassword = "123456";
+accountCity = "kolkata";
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "tan.moy@gmail.com";
+accountPassword = "12345678";
+accountCity = "Bata";
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+
+let name = "tanmoy";
+let age = 20;
+let isStudent = true;
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
